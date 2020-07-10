0abaff3585478f2d37ad2633685b605501377e1d
palme94 <68118826+palme94@users.noreply.github.com>
2020-07-10 17:40:44 +0200

    Add files via upload

---
+++ Newsdaten.js
@@ -0,0 +1,34 @@
+<script>
+Newsbox = new Array(
+
+  	
+	new Date(2020,6,30), "Sonstiges", 'https://www.hohenfurch.de/Sonstiges.17886.0.html#c80374', "Ferienprogramm", "am 22.08.2020", "Schnupperkurs &amp; Rundlauf",
+	new Date(2020,6,22), "Sonstiges", 'https://www.hohenfurch.de/Tischtennis.4173.0.html#c77273', "Anstehende Spiele", "Neu", "konfiguriert",
+	new Date(2020,6,21), "Sonstiges", 'https://www.hohenfurch.de/Meilensteine.18137.0.html#c77440', "Meilensteine", "Aktualiesierung", "Trainingslager",
+	new Date(2020,6,18), "Sonstiges", 'https://www.hohenfurch.de/Tischtennis.4173.0.html#c77469', "Willkommenstext", "diverse", "Änderungen",
+	new Date(2020,6,18), "Info", 'https://www.hohenfurch.de/Aktuelles-Saison-19-20.17293.0.html#c80327', "Trainingslager", "2020", "fällt aus",
+	new Date(2020,6,9), "Jugend", 'https://www.hohenfurch.de/Jugend.17780.0.html#c80296', "Jugend", "", "Abschlussbericht",
+  	new Date(2020,5,26), "Sonstiges", 'https://www.hohenfurch.de/Tischtennis.4173.0.html#c77469', "Willkommenstext", "Anpassung an", "neue Struktur",
+	new Date(2020,5,26), "Sonstiges", 'https://www.hohenfurch.de/Meilensteine.18137.0.html', "Vereinschronik", "verschoben", "in neuem Reiter",
+	new Date(2020,3,12), "Herren", 'https://www.hohenfurch.de/Herren.17779.0.html#c79147', "Herren", "SVH kann", "wieder siegen!",
+  	new Date(2020,3,10), "Jugend", 'https://www.hohenfurch.de/Jugend.17780.0.html#c79097', "Jugend", "Perfektes Spiel", "gegen Söchering II",
+  	new Date(2020,3,9), "Sonstiges", 'https://www.hohenfurch.de/Sonstiges.17886.0.html#c79093', "Altpapiersammlung", "vom 07.03.2020", "Bericht",
+	new Date(2020,3,5), "Herren", 'https://www.hohenfurch.de/Herren.17779.0.html#c79043', "Herren", "SVH geht gegen", "TSV Penzberg II baden",
+  	new Date(2020,2,18), "Jugend", 'https://www.hohenfurch.de/Jugend.17780.0.html#c78892', "Jugend", "Tolle Leistung", "gegen Schongau III",
+  	new Date(2020,2,17), "Jugend", 'https://www.hohenfurch.de/Jugend.17780.0.html#c78882', "Jugend", "Großweil II eine", "Nummer zu groß",
+  	new Date(2020,2,3), "Herren", 'Herren.17779.0.html#c78805', "Herren", "Gutes Spiel wird", "nicht belohnt",
+  	new Date(2020,2,3), "Jugend", 'Jugend.17780.0.html#c78801', "Jugend", "2 Punkte trotz", "klarer Niederlage",
+  	new Date(2019,11,05), "Info", 'Aktuelles-Saison-19-20.17293.0.html#c78080', "Trainingslager", "Ausschreibung für","den Sommer 2020",
+  
+  );
+
+
+Newsboxvorlage = new Array (
+	new Date(2020,6,30), "Sonstiges", 'URL', "Überschrift", "Info1", "Info2",
+	new Date(2020,6,30), "Info", 'URL', "Überschrift", "Info1", "Info2",
+	new Date(2020,6,30), "Herren", 'URL', "Herren", "Info1", "Info2",
+  	new Date(2020,6,30), "Jugend", 'URL', "Jugend I", "Info1", "Info2",
+  	new Date(2020,6,30), "JugendII", 'URL', "Jugend II", "Info1", "Info2",
+  
+);
+</script>
